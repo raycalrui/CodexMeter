661a1a2eb804a597a47853994ec2d9e0f7aa18a7
raycalrui <raycalrui@gmail.com>
2026-08-05 03:52:47 +0800

    Initial Commit

---
+++ CodexMeter/CodexMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexMeterApp.swift
+//  CodexMeter
+//
+//  Created by raycal on 8/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
